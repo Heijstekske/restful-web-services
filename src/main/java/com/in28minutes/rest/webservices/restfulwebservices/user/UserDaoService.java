71618e0fe5e782fe7ddfb0548f3023570d538c42
Heijstekske <heijstekker@gmail.com>
2023-02-17 15:39:43 +0100

    findById added for /users.{id} pathvariable

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -13,10 +13,12 @@ public class UserDaoService {
 
    private static List<User> users = new ArrayList<>();
 
+   private static int usersCount = 0;
+
    static {
-      users.add(new User(1, "Niels", LocalDate.of(1973,8,8)));
-      users.add(new User(2, "Heleen", LocalDate.of(1970,7,22)));
-      users.add(new User(3, "Kaysri", LocalDate.of(1970,10,21)));
+      users.add(new User(++usersCount, "Niels", LocalDate.of(1973,8,8)));
+      users.add(new User(++usersCount, "Heleen", LocalDate.of(1970,7,22)));
+      users.add(new User(++usersCount, "Kaysri", LocalDate.of(1970,10,21)));
    }
 
    public List<User> findAll(){
@@ -26,5 +28,11 @@ public class UserDaoService {
    public User findById(int id){
       Predicate<? super User> predicate = user -> Objects.equals(user.getId(), id);
       return users.stream().filter(predicate).findFirst().get();
+   }
+
+   public User save(User user){
+      user.setId(++usersCount);
+      users.add(user);
+      return user;
    }
 }
